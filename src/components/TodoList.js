c26bc84b89b1e440f082c5bf30b2c5dbdd5401aa
DanielC34 <125518197+DanielC34@users.noreply.github.com>
2023-09-07 23:00:19 +0300

    Updated TodoList.js
    Refactored the TodoList component to use Redux action creators for marking todos as complete, editing todo titles, and deleting todos.  Removed previous code that directly dispatched actions and used constants for action types

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,114 +1,46 @@
-import React, { Component } from 'react';
+// Import React and necessary dependencies
+import React from 'react';
 import { connect } from 'react-redux';
-import TodoItem from './TodoItem';
+import TodoItem from './TodoItem'; // Import TodoItem component
+import { markTodoCompleteAction, editTodoAction, deleteTodoAction } from '../redux/actions'; // Import Redux actions
 
-// Define action types as constants
-const ADD_TODO = 'ADD_TODO';
-const TOGGLE_TODO = 'TOGGLE_TODO';
-const EDIT_TODO = 'EDIT_TODO';
-const DELETE_TODO = 'DELETE_TODO';
+// Define a functional component named TodoListComponent
+function TodoListComponent(props) {
+    // Destructure props to access todos, markTodoCompleteAction, editTodoAction, and deleteTodoAction
+    const { todos, markTodoCompleteAction, editTodoAction, deleteTodoAction } = props;
 
-class TodoList extends Component {
-  constructor(props) {
-    super(props);
-
-    // Initialize component state to manage the new todo text
-    this.state = {
-      newTodoText: '',
-    };
-  }
-
-  // Function to handle adding a new todo
-  handleAddTodo = () => {
-    const { newTodoText } = this.state;
-
-    // Checking if the input is not empty (whitespace trimmed)
-    if (newTodoText.trim() !== '') {
-      // Dispatching the 'ADD_TODO' action with the new todo text
-      this.props.dispatch({
-        type: ADD_TODO,
-        payload: {
-          title: newTodoText, // Pass the title in the payload
-          description: '', // Add the relevant description here
-          deadline: '', // Add the relevant deadline here
-        },
-      });
-      // Clear the input field by resetting the state
-      this.setState({ newTodoText: '' });
-    }
-  };
-
-  // Function to handle marking a todo as complete
-  handleToggleTodo = (id) => {
-    // Dispatch the 'TOGGLE_TODO' action with the todo's ID
-    this.props.dispatch({
-      type: TOGGLE_TODO,
-      payload: {
-        id,
-      },
-    });
-  };
-
-  // Function to handle editing a todo
-  handleEditTodo = (id, newText) => {
-    // Dispatch the 'EDIT_TODO' action with the todo's ID and new text
-    this.props.dispatch({
-      type: EDIT_TODO,
-      payload: {
-        id,
-        title: newText, // Pass the edited title in the payload
-      },
-    });
-  };
-
-  // Function to handle deleting a todo
-  handleDeleteTodo = (id) => {
-    // Dispatch the 'DELETE_TODO' action with the todo's ID
-    this.props.dispatch({
-      type: DELETE_TODO,
-      payload: {
-        id,
-      },
-    });
-  };
-
-  // Render method to display the component's UI
-  render() {
-    const { todos } = this.props;
-    const { newTodoText } = this.state;
-
+    // Render the TodoListComponent
     return (
-      <div className="todo-list">
-        <h2>Todo List</h2>
-        <div className="todo-input">
-          <input
-            type="text"
-            placeholder="Add a new task"
-            value={newTodoText}
-            onChange={(e) => this.setState({ newTodoText: e.target.value })}
-          />
-          <button onClick={this.handleAddTodo}>Add</button>
+        <div>
+            <h1>Todo List</h1>
+            
+            <ul>
+                {/* Map over the 'todos' array and create a TodoItem component for each todo */}
+                {todos.map((todo) => (
+                    <TodoItem
+                        key={todo.id}
+                        todo={todo}
+                        markAsComplete={markTodoCompleteAction} 
+                        editTodo={editTodoAction} 
+                        deleteTodo={deleteTodoAction} 
+                    />
+                ))}
+            </ul>
         </div>
-        <ul>
-          {/* Map through the 'todos' array and render 'TodoItem' components */}
-          {todos.map((todo) => (
-            <TodoItem
-              key={todo.id}
-              todo={todo}
-              onToggle={this.handleToggleTodo}
-              onEdit={this.handleEditTodo}
-              onDelete={this.handleDeleteTodo}
-            />
-          ))}
-        </ul>
-      </div>
     );
-  }
 }
 
-// Map the Redux state to props, specifically the 'todos' array
+// Map the Redux state (specifically, 'todos') to props
 const mapStateToProps = (state) => ({
-  todos: state.todos,
+    todos: state.todos, // Assuming 'todos' is the name of the slice of state in your store
 });
 
-export default connect(mapStateToProps)(TodoList);
+// Map Redux actions (markTodoCompleteAction, editTodoAction, deleteTodoAction) to props
+const mapDispatchToProps = {
+    markTodoCompleteAction, // Map the action creator for marking a todo as complete
+    editTodoAction, // Map the action creator for editing a todo
+    deleteTodoAction, // Map the action creator for deleting a todo
+};
+
+// Connect the component to the Redux store using mapStateToProps and mapDispatchToProps
+export default connect(mapStateToProps, mapDispatchToProps)(TodoListComponent);
