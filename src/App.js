e499a4475f2b0cb0d00ec6aec683834a669e711f
DanielC34 <125518197+DanielC34@users.noreply.github.com>
2023-09-09 02:32:22 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import AddTodo from './components/AddTodo';
-import TodoList from './components/TodoList'
-import './App.css';
+import TodoList from './components/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="App-title">My Tasks</h1>
-      <AddTodo />
-      <TodoList />
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div className="app">
+			<h1 className="app-title">My Tasks</h1>
+			<AddTodo />
+			<TodoList />
+		</div>
+	);
+};
 
 export default App;
