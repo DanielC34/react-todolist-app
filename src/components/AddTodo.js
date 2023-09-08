1a01c001c2f3174c9a62759cc0dd040d7fc3d23d
DanielC34 <125518197+DanielC34@users.noreply.github.com>
2023-09-09 01:56:18 +0300

    Update AddTodo.js

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,57 +1,72 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { addTodoAction } from '../redux/actions';
+// Import necessary libraries and dependencies
+import React, { useState } from 'react';
 
-class AddTodo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todoText: '', // Initialize the input field value
-    };
-  }
+// Import the 'useDispatch' hook from 'react-redux' to access the Redux store's dispatch function
+import { useDispatch } from "react-redux";
 
-  render() {
-    return (
-      <div>
-        <input
-          type="text"
-          placeholder="Enter your task"
-          value={this.state.todoText}
-          onChange={this.handleInputChange}
-        />
-        <button onClick={this.handleAddTodo}>Add</button>
-      </div>
-    );
-  }
+// Import the 'addTask' action creator from the 'taskSlice' Redux slice
+import { addTask } from "../redux/taskSlice";
 
-  // Define event handler for input change
-  handleInputChange = (e) => {
-    this.setState({ todoText: e.target.value });
-  };
+// Define the 'AddTodo' component
+const AddTodo = () => {
+    // Initialize a state variable 'value' using useState to manage the input field's value
+	const [value, setValue] = useState('');
 
-  // Define event handler for adding a todo
-  handleAddTodo = () => {
-    // Get the todo text from the input field
-    const todo = this.state.todoText;
+    // Access the 'dispatch' function from the Redux store
+	const dispatch = useDispatch();
 
-    // Dispatch an action to add the todo to the Redux store
-    this.props.addTodoAction(todo);
+    // Define a function 'onSubmit' to handle form submission
+	const onSubmit = (event) => {
+        // Prevent the default form submission behavior
+		event.preventDefault();
 
-    // Clear the input field after adding the todo
-    this.setState({ todoText: '' });
-  };
-}
+        // Check if the input value is empty or only contains whitespace
+		if (value.trim().length === 0) {
+            // Display an alert if the input is empty
+			alert("Enter a task before adding !!");
+            
+            // Clear the input field by resetting the 'value' state
+			setValue("");
+            
+            // Exit the function to prevent further execution
+			return;
+		}
 
-const mapStateToProps = (state) => {
-  return {
-    // Map any relevant state to props here (if needed)
-  };
+        // Dispatch the 'addTask' action with the task description as payload
+		dispatch(
+			addTask({
+				task: value
+			})
+		);
+
+        // Clear the input field by resetting the 'value' state
+		setValue("");
+	};
+
+    // Render the component's UI
+	return (
+		<div className="add-todo">
+            {/* Input field for entering a new task */}
+			<input
+				type="text"
+				className="task-input"
+				placeholder="Add task"
+				value={value}
+                // Update the 'value' state when the input changes
+				onChange={(event) => setValue(event.target.value)}
+			></input>
+
+            {/* Button to submit the new task */}
+			<button
+                className="task-button"
+                // Call the 'onSubmit' function when the button is clicked
+				onClick={onSubmit}
+			>
+				Save
+			</button>
+		</div>
+	);
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    addTodoAction: (title) => dispatch(addTodoAction(title)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(AddTodo);
+// Export the 'AddTodo' component
+export default AddTodo;
