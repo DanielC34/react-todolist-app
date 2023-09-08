5de37ed0cb9b242faa695615bbd08fa457562384
DanielC34 <125518197+DanielC34@users.noreply.github.com>
2023-09-09 01:47:49 +0300

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,46 +1,34 @@
-// Import React and necessary dependencies
+// Import necessary libraries and dependencies
 import React from 'react';
-import { connect } from 'react-redux';
-import TodoItem from './TodoItem'; // Import TodoItem component
-import { markTodoCompleteAction, editTodoAction, deleteTodoAction } from '../redux/actions'; // Import Redux actions
 
-// Define a functional component named TodoListComponent
-function TodoListComponent(props) {
-    // Destructure props to access todos, markTodoCompleteAction, editTodoAction, and deleteTodoAction
-    const { todos, markTodoCompleteAction, editTodoAction, deleteTodoAction } = props;
+// Import the 'TodoItem' component
+import TodoItem from './TodoItem';
 
-    // Render the TodoListComponent
+// Import the 'useSelector' hook from 'react-redux' to access the Redux store
+import { useSelector } from "react-redux";
+
+// Define the 'TodoList' component
+const TodoList = () => {
+    // Use the 'useSelector' hook to retrieve the 'tasks' state from the Redux store
+    const todos = useSelector((state) => {
+        return state.tasks;
+    });
+
+    // Render the component's UI
     return (
-        <div>
-            <h1>Todo List</h1>
-            
-            <ul>
-                {/* Map over the 'todos' array and create a TodoItem component for each todo */}
-                {todos.map((todo) => (
-                    <TodoItem
-                        key={todo.id}
-                        todo={todo}
-                        markAsComplete={markTodoCompleteAction} 
-                        editTodo={editTodoAction} 
-                        deleteTodo={deleteTodoAction} 
-                    />
-                ))}
-            </ul>
-        </div>
+        <ul className="tasks-list">
+            {/* Map over the 'todos' array and render 'TodoItem' components for each task */}
+            {todos.map((todo) => (
+                <TodoItem
+                    // Pass task properties (id, title, completed) as props to 'TodoItem'
+                    id={todo.id}
+                    title={todo.name}
+                    completed={todo.status}
+                />
+            ))}
+        </ul>
     );
-}
-
-// Map the Redux state (specifically, 'todos') to props
-const mapStateToProps = (state) => ({
-    todos: state.todos, // Assuming 'todos' is the name of the slice of state in your store
-});
-
-// Map Redux actions (markTodoCompleteAction, editTodoAction, deleteTodoAction) to props
-const mapDispatchToProps = {
-    markTodoCompleteAction, // Map the action creator for marking a todo as complete
-    editTodoAction, // Map the action creator for editing a todo
-    deleteTodoAction, // Map the action creator for deleting a todo
 };
 
-// Connect the component to the Redux store using mapStateToProps and mapDispatchToProps
-export default connect(mapStateToProps, mapDispatchToProps)(TodoListComponent);
+// Export the 'TodoList' component
+export default TodoList;
